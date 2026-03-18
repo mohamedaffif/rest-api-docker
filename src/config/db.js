3f08633aac3d81cb2905772ce679475f25288f48
motanafi <twaha2030@gmail.com>
2026-03-18 03:29:28 +0300

    DATABASE CONFIG

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,19 @@
-import pkg from 'pg';
-const {Pool} = pkg;
-import dotenv from 'dotenv';
+import pkg from "pg";
+const { Pool } = pkg;
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: 5432
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+});
 
-pool.on('connect', () => {
-    console.log('Connected to the database');
-})
+pool.on("connect", () => {
+  console.log("Connected to the database");
+});
 
 export default pool;
