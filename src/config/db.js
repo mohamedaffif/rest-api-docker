9231fe1a385854f85e8f68c23620abee561a819f
motanafi <twaha2030@gmail.com>
2026-03-18 03:34:06 +0300

    added guard

--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("❌ DATABASE_URL is not defined");
+}
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl:
